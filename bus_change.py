c3a3d42c9162776eb5106667d8b0ca8c9dfab28e
Dheeraj Kumar <dheeraj@RAHULKUMAR.local>
2026-02-10 14:41:10 +0530

    Initial code

---
+++ bus_change.py
@@ -0,0 +1,19 @@
+passenger = []
+passenger.append(int(input("Passenger 1: ")))
+passenger.append(int(input("Passenger 2: ")))
+passenger.append(int(input("Passenger 3: ")))
+passenger.append(int(input("Passenger 4: ")))
+passenger.append(int(input("Passenger 5: ")))
+valid = [5,10,20,50,100]
+for i in passenger:
+    if i in valid:
+        print()
+    else:
+        print(f"{i} is Invalid")
+        break
+if sum(passenger)>50:
+    print("False")
+elif passenger.count(10)>2:
+    print("False")
+else:
+    print("True")
